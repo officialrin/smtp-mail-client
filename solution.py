17755879f105555696572cc3bb8786dbfd54e129
rin <75649277+officialrin@users.noreply.github.com>
2021-12-09 02:36:51 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    clientSocket.connect(mailserver)
 
    recv = clientSocket.recv(1024).decode()
-   print(recv)
+   # print(recv)
    if recv[:3] != '220':
        #print('220 reply not received from server.')
 
@@ -20,7 +20,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    heloCommand = 'HELO Alice\r\n'
    clientSocket.send(heloCommand.encode())
    recv1 = clientSocket.recv(1024).decode()
-   print(recv1)
+   # print(recv1)
    if recv1[:3] != '250':
        #print('250 reply not received from server.')
 
@@ -68,7 +68,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    # Send QUIT command and get server response.
    clientSocket.send("QUIT\r\n".encode())
    recv1 = clientSocket.recv(1024)
-   print(recv1)
+   # print(recv1)
    clientSocket.close()
 
 if __name__ == '__main__':
