7741954f7ac329d894c7a1d5f6ab81a106a0c665
rin <75649277+officialrin@users.noreply.github.com>
2021-12-10 19:30:52 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
    clientSocket = socket(AF_INET, SOCK_STREAM)
-   clientSocket.connect(mailserver, port)
+   clientSocket.connect((mailserver, port))
 
    recv = clientSocket.recv(1024)
  # print(recv)
@@ -68,7 +68,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    # Send QUIT command and get server response.
    clientSocket.send("QUIT\r\n".encode())
    recv7 = clientSocket.recv(1024)
-   # print(recv7)
+  # print(recv7)
    clientSocket.close()
 
 if __name__ == '__main__':
